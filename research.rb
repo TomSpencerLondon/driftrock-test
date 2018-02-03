063b789e4365c0fc1c772bbfe9b2a79d3e303e1a
tomspencerlondon <tomspencer_uk@yahoo.co.uk>
2018-02-03 19:46:09 +0000

    Add find_id method

--- research.rb
+++ research.rb
@@ -1,23 +1,46 @@
 require_relative 'app'
 require 'json'
 
-def find_email(requested_id)
+def api_request_enumerator(api_request_proc, per_page = 2000)
   page_number = 1
-  per_page = 20
   reached_end = false
 
-  until reached_end
-    object = JSON.parse(APIRequest.users(page_number, per_page))
-    array = object['data']
+  Enumerator.new do |enum|
+    until reached_end
+      object = JSON.parse(api_request_proc.call(page_number, per_page))
+      array = object['data']
 
-    array.each do |hash|
-      return hash['email'] if hash['id'] == requested_id
+      array.each do |hash|
+        enum.yield(hash)
+      end
+      page_number += 1
+      reached_end = array.length < per_page
     end
-    page_number += 1
-    reached_end = array.length < per_page
   end
 
 end
 
+def find_email(requested_id)
+  api_request_proc = Proc.new{ |page_number, per_page| APIRequest.users(page_number, per_page) }
+
+  enum = api_request_enumerator(api_request_proc)
+  enum.each do |hash|
+    return hash['email'] if hash['id'] == requested_id
+  end
+end
+
 user_email = find_email("2K7H-A8SQ-6R5V-8OT5")
 p user_email
+
+def find_id(requested_email)
+  api_request_proc = Proc.new{ |page_number, per_page| APIRequest.users(page_number, per_page) }
+
+  enum = api_request_enumerator(api_request_proc)
+  enum.each do |hash|
+    return hash['id'] if hash['email'] == requested_email
+  end
+end
+
+user_id = find_id("purdy_elva@bahringer.net")
+
+p user_id
